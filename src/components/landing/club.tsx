0c6746a9ee095cfb112c4b96a498deabfeca20cd
CryptoNinja1210 <crypto.ninja1210@gmail.com>
2023-12-23 00:55:41 +0900

    fix1

--- src/components/landing/club.tsx
+++ src/components/landing/club.tsx
@@ -11,7 +11,7 @@ export default function Welcome () {
         <div className="tw-bg-[url('/images/bg/shape-c-right-gray.svg')] tw-h-[880px] tw-bg-bottom">
           <div className='tw-bg-club-gradient tw-h-[880px] tw-items-center'>
             <div className=' tw-flex tw-flex-col tw-justify-center tw-max-w-[1098px] tw-h-[880px] tw-m-auto sm:tw-p-5 tw-p-0 tw-items-center'>
-              <div className=' tw-font-Antique tw-text-center tw-text-white tw-text-[2rem] lg:tw-text-[4rem] tw-font-bold tw-leading-[90%]'>Don't miss out on this amazing opportunity! Sign up today and let's learn, connect, and have fun together!</div>
+              <div className=' tw-font-Antique tw-text-center tw-text-white tw-text-[2rem] lg:tw-text-[4rem] tw-font-bold tw-leading-[90%]'>Don't miss out on this amazing opportunity!<br />Sign up today and let's learn, connect, and have fun together!</div>
               <div className=' tw-mt-8 tw-font-Reckless tw-text-lg tw-text-[18px] tw-leading-[140%] tw-max-w-[832px] tw-text-white tw-text-center tw-pt-14'>
                 Are you eager to practice a new language with people from diverse countries? Look no further! Our Language Speaking Club is the perfect platform for language enthusiasts like you.
               </div>
